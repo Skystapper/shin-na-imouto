fdc4636cfdb292c3a24d78994be5cf5c28367216
Skystapper <97273031+Skystapper@users.noreply.github.com>
2022-12-03 11:30:39 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,4 @@
-#(©)Codexbotz
+#(漏)Codexbotz
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>⛩️ My Beloved Onee-Chan : \n    <a href='https://t.me/hanimeverse'>🍥Ecchi Senpai🍥</a>\n🎋 My Other Siblings : \n  <a href='https://t.me/+B2imbmmmuR84OTJl'>🍡3D-Waifus</a>\n    <a href='https://t.me/+3jP7uPeF43s4MDY1'>🍡Cosplay Kouhai</a>\n      <a href='https://t.me/Hanimeland_requestbot'>🍡Erome-San</a>\n🎍 My Family : <a href='https://t.me/Notice_meSenpai'>Notice Me?Senpai!!!</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("🎐 Close", callback_data = "close")
                     ]
                 ]
             )
